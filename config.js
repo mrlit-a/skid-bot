46f7f8bd7a6dd711b982986aa72f20c9588d018f
skid <112231743+Skidy89@users.noreply.github.com>
2023-07-11 12:14:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.ask = fs.readFileSync('./media/query.jpg')
 global.dumb = fs.readFileSync('./media/dumb.jpg')
 global.error = fs.readFileSync('./media/error.jpg')
 
-
+global.fdoc = { key : { participant : '0@s.whatsapp.net' }, message: { documentMessage: { title: `puto`, jpegThumbnail: ask }}}
 
     
 
