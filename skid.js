d7b3d0ab6995641fc6c7d24f037149f59f0910bc
skid <112231743+Skidy89@users.noreply.github.com>
2023-07-18 22:11:17 -0600

    Update skid.js

--- skid.js
+++ skid.js
@@ -378,7 +378,10 @@ case 'bloodfrosted':
 case 'blackpink':
 case 'pornhub':
 if (!text) { m.reply('test')}
+try {
 lol = `https://api.lolhuman.xyz/api/textprome/${command}?apikey=${lolkeysapi}&text=${text}`
+} catch {
+lol = `https://api.lolhuman.xyz/api/textprome2/${command}?apikey=${lolkeysapi}&text=${text}`
 sendImageAsUrl(lol, `aqui esta su texto en estilo ${command}`)
 break
         case 'hidetag': {
