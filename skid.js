32ffd12f82454cb66e3354a7fd273fec57cfcbd9
skid <112231743+Skidy89@users.noreply.github.com>
2023-07-29 20:50:21 -0300

    Actualizar skid.js

--- skid.js
+++ skid.js
@@ -560,18 +560,14 @@ case 'getcase':
     const searchcomand = args[0].toLowerCase()
     const commands = fs.readFileSync('./skid.js').toString()
 
-    const caseRegex = CASE_SENSITIVITY ? new RegExp(`case '(\\w*${searchcomand}\\w*)'`, 'g') : new RegExp(`case '(\\w*${searchcomand}\\w*)'`, 'gi')
+    const caseRegex = CASE_SENSITIVITY ? new RegExp(`case '(\\w*${searchcomand}\\w*)'(.*?)(break;|})`, 'gs') : new RegExp(`case '(\\w*${searchcomand}\\w*)'(.*?)(break;|})`, 'gis')
     const similarsk = commands.match(caseRegex)
 
     if (!similarsk || similarsk.length === 0) {
       reply(`Error, tal vez no existe el ${searchcomand}.\nNo se encontraron comandos similares.`)
     } else {
-      const diegogay = similarsk.slice(0, MAX_SIMILAR_CASES).map(caseText => caseText.split(`case '`)[1].split(`'`)[0])
-      if (diegogay.includes(searchcomand)) {
-        reply(`*Se encontró el comando ${searchcomand}*\n${similarsk[0]}`)
-      } else {
-        reply(`*No se encontró el comando ${searchcomand}*\n*Aquí tienes una lista de comandos similares:*\n${diegogay.join('\n')}`)
-      }
+      const suggestions = similarsk.slice(0, MAX_SIMILAR_CASES).map(caseText => caseText.trim())
+      reply(`*Se encontró el comando ${searchcomand}*\n${suggestions.join('\n')}`)
     }
   } catch (err) {
     console.error(err)
