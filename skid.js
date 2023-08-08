bfe21feccc1ff0ea097efa2245404f5d52886dd2
skid <112231743+Skidy89@users.noreply.github.com>
2023-08-08 02:01:51 -0600

    Actualizar skid.js

--- skid.js
+++ skid.js
@@ -502,14 +502,6 @@
  break 
   
   
-case 'fake':
-  		var gh = body.slice(11)
-		mentioned = m.message.extendedTextMessage.contextInfo.mentionedJid
-		var replace = gh.split("|")[0];
-		var target = gh.split("|")[1];
-		var bot = gh.split("|")[2];
-		conn.fakeReply(m.chat, `${bot}`, `${target}`, `${replace}`)
-		break
 
   case 'bass': case 'blown': case 'deep': case 'earrape': case 'fast': case 'fat': case 'nightcore': case 'reverse': case 'robot': case 'slow': case 'smooth': case 'squirrel':  
                   try {  
