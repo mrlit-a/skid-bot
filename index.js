d040f7e978d756e057dc1845bc99eb9bbade9d60
skid <112231743+Skidy89@users.noreply.github.com>
2023-08-08 14:04:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ sock.ev.on('messages.upsert', async chatUpdate => {
     mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
     if (mek.key && mek.key.remoteJid === 'status@broadcast') return
     
-    if (!conn.public && !m.key.fromMe && !chatUpdate.type === 'notify') return
+    if (!sock.public && !m.key.fromMe && !chatUpdate.type === 'notify') return
     m = smsg(sock, mek)
     //if (m.key.fromMe === true) return
     //if (m.mtype === 'senderKeyDistributionMessage') mek = chatUpdate.messages[1]
@@ -125,7 +125,7 @@ sock.ev.on('connection.update', async (update) => {
     }
 });
 
-conn.public = true
+sock.public = true
 
 sock.ev.on('creds.update', saveCreds)
 
