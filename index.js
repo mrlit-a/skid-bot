de4a88db3205bb9d4a99ae5c9fc7940007a0cd6f
skid <112231743+Skidy89@users.noreply.github.com>
2023-08-09 03:35:51 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -57,8 +57,36 @@ const sock = makeWASocket({
     logger: pino({ level: 'silent' }),
     browser: [`skid bot`,'Safari','3.0']
 })
-let chats = global.db.data.chats[m.chat]
 
+
+sock.ev.on('messages.upsert', async chatUpdate => {
+    //console.log(JSON.stringify(chatUpdate, undefined, 2))
+    try {
+    chatUpdate.messages.forEach(async (mek) => {
+    try {
+    //mek = (Object.keys(chatUpdate.messages[0])[0] !== "senderKeyDistributionMessage") ?  chatUpdate.messages[0] : chatUpdate.messages[1]
+
+    if (!mek.message) return
+    //console.log(chatUpdate.type)
+    mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+    
+    if (!sock.public && !m.key.fromMe && !chatUpdate.type === 'notify') return
+    m = smsg(sock, mek)
+    let chats = global.db.data.chats[m.chat]
+    //if (m.key.fromMe === true) return
+    //if (m.mtype === 'senderKeyDistributionMessage') mek = chatUpdate.messages[1]
+    global.numBot = sock.user.id.split(":")[0] + "@s.whatsapp.net"
+    global.numBot2 = sock.user.id
+    require("./skid")(sock, m, chatUpdate, mek)
+    } catch (e) {
+    console.log(e)
+    }
+    })
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 // adaptado por skid
 if (global.db.data.chats[m.chat].welcome) {
@@ -181,34 +209,6 @@ console.log(err)
 }
 })
 }
-
-sock.ev.on('messages.upsert', async chatUpdate => {
-    //console.log(JSON.stringify(chatUpdate, undefined, 2))
-    try {
-    chatUpdate.messages.forEach(async (mek) => {
-    try {
-    //mek = (Object.keys(chatUpdate.messages[0])[0] !== "senderKeyDistributionMessage") ?  chatUpdate.messages[0] : chatUpdate.messages[1]
-
-    if (!mek.message) return
-    //console.log(chatUpdate.type)
-    mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
-    
-    if (!sock.public && !m.key.fromMe && !chatUpdate.type === 'notify') return
-    m = smsg(sock, mek)
-    //if (m.key.fromMe === true) return
-    //if (m.mtype === 'senderKeyDistributionMessage') mek = chatUpdate.messages[1]
-    global.numBot = sock.user.id.split(":")[0] + "@s.whatsapp.net"
-    global.numBot2 = sock.user.id
-    require("./skid")(sock, m, chatUpdate, mek)
-    } catch (e) {
-    console.log(e)
-    }
-    })
-    } catch (err) {
-        console.log(err)
-    }
-})
 
 sock.ev.on('connection.update', async (update) => {
     const { connection, lastDisconnect, qr, receivedPendingNotifications } = update;
